1bb95febf849c9be0c2c5fdec27ee5b2c1655829
Niteshkumar Shah <nshah@loandepot.com>
2022-12-22 21:24:54 -0600

    first commit

---
+++ DatatableToCSV.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL2EXCEL
+{
+    internal class DatatableToCSV
+    {
+        public void write(DataTable dt, String location)
+        {
+            var lines = new List<string>();
+
+            string[] columnNames = dt.Columns
+                .Cast<DataColumn>()
+                .Select(column => column.ColumnName)
+                .ToArray();
+
+            var header = string.Join(",", columnNames.Select(name => $"\"{name}\""));
+            lines.Add(header);
+
+            var valueLines = dt.AsEnumerable()
+                .Select(row => string.Join(",", row.ItemArray.Select(val => $"\"{val}\"")));
+
+            lines.AddRange(valueLines);
+
+            
+
+            File.WriteAllLines(location + "melloFIN_" + DateTime.Now.ToString("MMddyyyy_hhmm")  + ".csv", lines);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+using System.Data;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using SQL2EXCEL;
+using System.Xml.Linq;
+
+DatatableToCSV toCSV = new DatatableToCSV();
+
+// load the configuration file.
+var configBuilder = new ConfigurationBuilder().
+   AddJsonFile("appsettings.json").Build();
+
+IConfigurationRoot _config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false)
+    .Build();
+
+// get the section to read  
+var appSettings = configBuilder.GetSection("AppSettings");
+var fileLocation = appSettings["FileLocation"] ?? "c:\\";
+
+
+
+var sql = configBuilder.GetSection("AppSettings:query").Get<List<string>>();
+
+//sql.Join(",");
+var sqlString = String.Join("\n", sql);
+
+var connectionStrings = configBuilder.GetSection("ConnectionStrings");
+var defaultConnection = connectionStrings["DefaultConnection"] ?? null;
+
+SqlConnection connection = new SqlConnection();
+
+connection.ConnectionString = defaultConnection;
+SqlCommand command = new SqlCommand();
+command.CommandText = sqlString;
+
+using (SqlDataAdapter dataAdapter = new SqlDataAdapter(command.CommandText, connection))
+{
+    DataTable t = new DataTable();
+    dataAdapter.Fill(t);
+    toCSV.write(t, fileLocation);
+}
